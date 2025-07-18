ba335213890907a4b22943a5aeffe6b161eb9419
Harold Sorel <hsorel@mi-r4-p4.s19.be>
2025-07-18 21:00:31 +0200

    added chown

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -6,12 +6,11 @@ chown -R mysql:mysql /run/mysqld
 # Initialize DB if empty
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
+    chown -R mysql:mysql /var/lib/mysql
     mysql_install_db --user=mysql --ldata=/var/lib/mysql
-
     echo "Running initial SQL script..."
-    mysqld --user=mysql --bootstrap < /etc/mysql/init.sql
-
+    mysqld --user=mysql --bootstrap < conf/init.sql
 fi
 
-# Start MariaDB in foreground
+chmod -R 777 /var/lib/mysql
 exec mysqld --user=mysql --console
