d34f4aaf2c692bf535dbfd5fe760fa745aa79c35
Harold Sorel <hsorel@mi-r5-p1.s19.be>
2025-07-21 21:39:56 +0200

    edit typo

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -39,4 +39,7 @@ chmod +x wp-cli.phar
 #./wp-cli.phar option update siteurl http://localhost:8080 --allow-root
 #./wp-cli.phar option update home http://localhost:8080 --allow-root
 
+#chown -R wordpress:wordpress /var/www/html
+#chmod -R 755 /var/www/html
+
 exec php-fpm8.2 -F
