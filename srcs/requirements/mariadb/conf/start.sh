623d772a77b1c841ec22843e0c289b7827e7f84e
Harold Sorel <hsorel@mi-c2-p6.s19.be>
2025-07-23 21:57:52 +0200

    added comments

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -6,47 +6,74 @@
 #but when mariaDB will run it will do so as a dedicated linux user called mysql
 #this is why there is chown mysql:mysql so when MariaDB will run it will have correct permissions 
 
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 # Disable bind-address to allow external connections
 
-# Ensure runtime dir exists with correct perms
+
+# Ensure runtime directory exists with correct permissions
+# MariaDB expects this and Debian doesn´t offer it so we do it ourselve
+# Mariadb runs as Daemon so change the ownership otherwise it will have a permission error
 if [ ! -d "/run/mysqld" ]; then
     mkdir -p /run/mysqld
     chown -R mysql:mysql /run/mysqld
-    chmod 755 /run/mysqld
 fi
 
-# If no internal mysql DB, then initialize
+#initialize databse
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     chown -R mysql:mysql /var/lib/mysql
-    mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
-    # Start MariaDB temporarily to change configuration
-    /usr/sbin/mysqld --user=mysql --skip-networking &
-    
-    # Wait for MariaDB to become responsive
-    until mysqladmin ping --silent; do
-        printf 'waiting for MariaDB\n'
+    #initialization of the Mariadb directory (VOLUME!)
+    #assigns ownership to mysql (mariadb daemon)
+    mysql_install_db > dev/null #--user=mysql --datadir=/var/lib/mysql > dev/null
+
+    #this part starts mariaDB but just to change the users
+    #start MariaDB in the background quietly as mysql
+    #skip networking so that wordpress doesnt connect to it before the modification
+    /usr/sbin/mysqld --skip-networking > dev/null & 
+
+    # Wait for MariaDB to be ready before entering commands
+    until mysqladmin ping --user=${MARIADB_USER} --silent; do
         sleep 1
     done
 
-    #SQL setup
-    printf 'Setting up database...\n'
-    mariadb -u root <<EOF
-    DROP DATABASE IF EXISTS test;
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
-    DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-    DELETE FROM mysql.user WHERE user='';
-    CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\` CHARACTER SET utf8 COLLATE utf8_general_ci;
-    CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${MARIADB_NAME}\`.* TO '${MARIADB_USER}'@'%';
-    FLUSH PRIVILEGES;
-EOF
+    # SQL setup
+    echo -e "${YELLOW}Setting up database...${RESET}"
 
-    printf 'Database Set!\n'
+    echo -e "${BLUE}Setting root password for 'root'@'localhost'...${RESET}"
+    mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
 
-    # Shutdown temp MariaDB to lock-in new configuration
-    mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
-fi
+    echo -e "${BLUE}Removes default database...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "DROP DATABASE IF EXISTS test;"
 
-chmod -R 777 /var/lib/mysql
-exec mysqld --user=mysql --console
+    echo -e "${BLUE}Deleting unsafe root users...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "DELETE FROM mysql.user WHERE user='';"
+
+    echo -e "${BLUE}Creating database '${MARIADB_NAME}'...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_NAME}\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
+
+    echo -e "${BLUE}Creating user '${MARIADB_USER}' for remote connections...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSWORD}';"
+
+    echo -e "${BLUE}Granting privileges to user '${MARIADB_USER}' on '${MARIADB_NAME}'...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${MARIADB_NAME}\`.* TO '${MARIADB_USER}'@'%';"
+
+    echo -e "${BLUE}Flushing privileges to apply changes...${RESET}"
+    mariadb -u root -p"${MARIADB_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+    echo -e "${GREEN}Database Set!${RESET}"
+
+    # Shut down MariaDB quietly to lock-in changes
+    mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown > dev/null
+    fi
+
+chmod -R 777 /var/lib/mysql #so i can easily remove it
+
+# Start the real server
+echo -e "${GREEN}Lauching Mariadb!${RESET}"
+exec mysqld --console
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 
-#wait for everything else to setup CHANGE THIS SOON
-sleep 20
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
 
-cd /var/www/html
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+    echo -e "${YELLOW}Wordpress is not installed. Installing...${RESET}"
 
-./wp-cli.phar core download --allow-root
+    cd /var/www/html
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
 
-#creates the wp-config.php (creates the database, in this case MariaDB credentials)
-#tells wordpress how to connect to mariaDB
-./wp-cli.phar config create \
-    --dbname=$MARIADB_NAME \
-    --dbuser=$MARIADB_USER \
-    --dbpass=$MARIADB_USER_PASSWORD \
-    --dbhost=mariadb \
-    --allow-root #by default wordpress runs as a user and not root but you are a root in  the container so just put this to avoid an error 
+    ./wp-cli.phar core download --allow-root
+    
+    until mysqladmin ping --host=mariadb --user=$MARIADB_USER --password=$MARIADB_USER_PASSWORD --silent; do
+        echo -e "${YELLOW}Waiting for MariaDB...${RESET}"
+        sleep 1
+    done
+    echo -e "${GREEN}Connection with mariaDB successfull!${RESET}"
+    #creates the wp-config.php (creates the database credentials, in this case MariaDB credentials)
+    ./wp-cli.phar config create \
+        --dbname=$MARIADB_NAME \
+        --dbuser=$MARIADB_USER \
+        --dbpass=$MARIADB_USER_PASSWORD \
+        --dbhost=mariadb \
+        --allow-root #by default wordpress runs as a user and not root but you are a root in  the container so just put this to avoid an error 
 
-#change localhost 8080 to 80!
-./wp-cli.phar core install \
-    --url=$DOMAIN_NAME \
-    --title="Inception" \
-    --admin_user=$WP_ADMIN_USER \
-    --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL \
-    --allow-root
+    #tells wordpress how to connect to mariaDB
+    #inserts some values like the title ect
+    ./wp-cli.phar core install \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --allow-root
 
-./wp-cli.phar user create \
-    $WP_USER \
-    $WP_USER_EMAIL \
-    --user_pass=$WP_USER_PASSWORD \
-    --role=author \
-    --allow-root #have a doubt on role editor
+    #creates a random user
+    ./wp-cli.phar user create \
+        $WP_USER \
+        $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PASSWORD \
+        --role=author \
+        --allow-root #have a doubt on role editor
+    #chown -R wordpress:wordpress /var/www/html
+else
+    echo -e "Wordpress is already installed!"
+fi
 
-#chown -R wordpress:wordpress /var/www/html
-#chmod -R 755 /var/www/html
+echo -e "${GREEN}Wordpress is Ready${RESET}!"
 
 exec php-fpm8.2 -F
